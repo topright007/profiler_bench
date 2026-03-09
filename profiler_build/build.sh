de5db4be60ffaf85357b14ef70580f0e6cc0321b
topright <topright@yandex-team.ru>
2026-03-09 14:59:09 +0300

    fix collector tests

--- profiler_build/build.sh
+++ profiler_build/build.sh
@@ -18,3 +18,20 @@ cd ..
 mkdir -p installer && rm -rf installer/*
 unzip -oq profiler_agent/installer/build/distributions/qubership-profiler-installer-3.1.3-SNAPSHOT.zip -d installer
 cp config.xml installer/config/default/75post/netcracker.generic.xml
+
+git clone https://github.com/topright007/qubership-profiler-backend.git profiler_backend || true
+cd profiler_backend
+git checkout strip_netcracker_ui_modules
+git pull
+cd apps/query
+npm install
+chmod +x build.sh && ./build.sh
+cp netcracker-cloud-profiler-ui-*.tgz ../collector/cloud-profiler-ui.tgz
+
+cd ../collector
+rm -rf target/cloud-profiler-ui || mkdir -p target/cloud-profiler-ui
+tar -xvzf cloud-profiler-ui.tgz -C target/cloud-profiler-ui \
+    && cp -fR target/cloud-profiler-ui/package/build/* src/main/resources/META-INF/resources/
+
+./mvnw --batch-mode clean package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -V -B
+docker build . -t "profiler-collector:latest" 
