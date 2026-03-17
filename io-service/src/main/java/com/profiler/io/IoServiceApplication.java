5940fed54a205574b4b634a88c63de063c7a165c
topright <elstano@gmail.com>
2026-03-17 22:08:49 +0000

    add otel

--- io-service/src/main/java/com/profiler/io/IoServiceApplication.java
+++ io-service/src/main/java/com/profiler/io/IoServiceApplication.java
@@ -3,19 +3,20 @@ package com.profiler.io;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class IoServiceApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(IoServiceApplication.class, args);
     }
-    
+
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
